ca8a70de88f9904e1c431dc3f48f5b563903575c
misohan <michal.hanusniak@aiesec.cz>
2020-07-01 00:23:37 +0200

    Update project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static <E> void main(String[] args) {
+    public static void main(String[] args) {
         String name ="Hello";
         int age =54;
         boolean bool = false;
         Student student = new Student("Michal", 58);
         Student student1 = new Student("Andrej", 43);
+        Student student2 = new Student("Ladislav", 43);
         List<Student> students = new ArrayList<Student>();
         List<Student> students1 = new ArrayList<Student>();
 
         students.add(student);
         students1.add(student);
 
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList();
-        List<E> listOfElements = recentlyUsedList.getListOfElements();
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(3);
 
-        recentlyUsedList.controller(listOfElements, student);
-        recentlyUsedList.controller(listOfElements, student);
+        recentlyUsedList.addObjectToList(student);
+        recentlyUsedList.addObjectToList(bool);
+        recentlyUsedList.addObjectToList(name);
+        recentlyUsedList.addObjectToList(age);
+        recentlyUsedList.addObjectToList(student);
+        recentlyUsedList.addObjectToList(student);
+        recentlyUsedList.addObjectToList(student);
+        recentlyUsedList.addObjectToList(student1);
+        recentlyUsedList.addObjectToList(name);
+        recentlyUsedList.addObjectToList(bool);
 
         System.out.println(recentlyUsedList.getListOfElements());
 
--- src/main/java/RecentlyUsedList.java
+++ src/main/java/RecentlyUsedList.java
@@ -1,126 +1,42 @@
 import java.util.*;
 
 public class RecentlyUsedList<E> {
-    private List listOfElements = new ArrayList<E>();
-    private static int sizeOfList = 3;
+    private ArrayList<E> listOfElements = new ArrayList<E>();
+    private int sizeOfList;
 
-
-//    public RecentlyUsedList(List listOfElements, int sizeOfList) {
-//        this.listOfElements = listOfElements;
-//        this.sizeOfList = sizeOfList;
-//    }
-
+    public RecentlyUsedList(int sizeOfList) {
+        this.sizeOfList = sizeOfList;
+    }
 
     public List<E> getListOfElements() {
         return listOfElements;
     }
 
     public void addObjectToList(E element){
-        checkIfDuplicate(element);
-        int index =0;
+
+        deleteDuplicate(element);
+
+        int index = 0;
         listOfElements.add(index,element);
-    }
 
-    public void getAllElements(List<E> listOfElements){
-        Iterator<E> eIterator = listOfElements.iterator();
-
-        while(eIterator.hasNext()){
-            E currentElement = eIterator.next();
-            printElementAndItsType(currentElement);
+        int currentSizeOfList = listOfElements.size();
+        if(sizeOfList<currentSizeOfList){
+            listOfElements.remove(currentSizeOfList-1);
         }
     }
+    
+    public void deleteDuplicate(E element) {
+        ListIterator<E> iterator = listOfElements.listIterator();
 
-    public List<E> removeDuplicateElements(List<E> listOfElements){
-        List<E> newListOfElements = new ArrayList<E>();
-        for(E element: listOfElements){
+        while(iterator.hasNext()){
+            if(iterator.next().getClass()==element.getClass()){
+                iterator.remove();
 
-            if(!newListOfElements.contains(element)){
-                newListOfElements.add(element);
             }
         }
-        return newListOfElements;
     }
-
-    public void printElementAndItsType(E element){
-        System.out.println(element);
-        System.out.println(element.getClass());
-    }
-
-    public boolean checkIfDuplicate(E element){
-        Iterator<E> eIterator = this.listOfElements.iterator();
-
-        while(eIterator.hasNext()){
-            E currentElement = eIterator.next();
-            if (currentElement.getClass() == element.getClass()){
-                return true;
-            }
-        }
-        return false;
-    }
-    public E returnDuplicateElement(E element){
-        Iterator<E> eIterator = this.listOfElements.iterator();
-
-        while(eIterator.hasNext()){
-            E currentElement = eIterator.next();
-            if (currentElement.getClass() == element.getClass()){
-                return currentElement;
-            }
-        }
-        return null;
-    }
-
-    public boolean checkSizeOfList(List<E> listOfElements){
-        if (listOfElements.size()<=sizeOfList){
-            return true;
-        }
-        return false;
-    }
-
-    public void controller(List<E> listOfElements, E element){
-        if (checkSizeOfList(listOfElements)){
-            if (!checkIfDuplicate(element)){
-                addObjectToList(element);
-            }
-            E duplicateElement = returnDuplicateElement(element);
-            listOfElements.remove(duplicateElement);
-
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
 
 
 
